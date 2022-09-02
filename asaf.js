f53514744ca9e999c18e0883aeeecdf87f157e2b
Chaim Cheskel Cik <102826569+ChezkyC@users.noreply.github.com>
2022-09-02 13:43:12 +0300

    new page for fun

---
+++ asaf.js
@@ -0,0 +1,8 @@
+let yes = document.getElementById(`yes1`)
+yes.addEventListener("click", () => {
+    document.body.style.backgroundImage = "url('i-love-you-hearts-balloons.gif')";
+    document.body.style.color = "blue"
+    yes.innerText = 'WILL YOU MARRY MY?'
+})
+
+
