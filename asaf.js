2da7ab52dfd952ab3b5186889f944a1becdcf3cf
Chaim Cheskel Cik <102826569+ChezkyC@users.noreply.github.com>
2022-09-02 14:25:50 +0300

    update with no btn

--- asaf.js
+++ asaf.js
@@ -1,8 +1,21 @@
 let yes = document.getElementById(`yes1`)
+let no = document.getElementById(`btn1`)
 yes.addEventListener("click", () => {
     document.body.style.backgroundImage = "url('i-love-you-hearts-balloons.gif')";
     document.body.style.color = "blue"
     yes.innerText = 'WILL YOU MARRY MY?'
+    no.innerHTML = no1()
 })
 
+no1 = () => {
+    no = document.getElementById(`btn`)
+    no.innerHTML = ""
+}
 
+
+
+const heSaidNo1 = () => {
+    document.body.style.backgroundImage = "url('sad.gif')";
+    document.body.innerHTML = ""
+}
+no.addEventListener(`click`, heSaidNo1)
